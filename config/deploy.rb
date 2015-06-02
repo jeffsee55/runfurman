598836c1eece7020e364d9da09e365455dbb36a9
Jeff See <jeffsee@Jeffs-MacBook-Air.local>
2015-06-02 17:23:08 -0400

    Cap config

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,10 +8,7 @@ set :repo_url, 'git@github.com:jeffsee55/runfurman.git'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
-
-# Default value for :scm is :git
-# set :scm, :git
+set :deploy_to, '/var/www'
 
 # Default value for :format is :pretty
 # set :format, :pretty
@@ -23,10 +20,10 @@ set :repo_url, 'git@github.com:jeffsee55/runfurman.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('.env')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, %w(media)
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
