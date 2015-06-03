388c48fc8e21d449195eb6ce41850dc8775c77a5
Jeff See <jeffsee@Jeffs-MacBook-Air.local>
2015-06-02 21:37:27 -0400

    Server config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-server "/var/www", user: "deploy", roles: %w{web app db}
+server "45.55.175.72:80", user: "deploy", roles: %w{web app db}
 
 
 # role-based syntax
