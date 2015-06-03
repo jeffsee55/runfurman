bae124bdd9e1ea008bd79f40c2232c5d6dd78da5
Jeff See <jeffsee@Jeffs-MacBook-Air.local>
2015-06-03 11:30:21 -0400

    Server

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'git@github.com:jeffsee55/runfurman.git'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/var/www'
+set :deploy_to, '/var/www/runfurman'
 
 # Default value for :format is :pretty
 # set :format, :pretty
